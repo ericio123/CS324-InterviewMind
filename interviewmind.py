109b8ad7c5df85607e81c5e0fe0748969c0fca7a
Eric Zeng <47470180+ericio123@users.noreply.github.com>
2023-03-10 07:56:33 -0800

    Added pdf parser input

--- interviewmind.py
+++ interviewmind.py
@@ -1,14 +1,8 @@
 import streamlit as st
 import openai
+import pdfplumber
 from streamlit.components.v1 import html
 st.set_page_config(page_title="InterviewMind")
-
-
-html_temp = """
-                <div style="background-color:{};padding:1px">
-                
-                </div>
-                """
 
 with st.sidebar:
     st.markdown("""
@@ -16,55 +10,42 @@ with st.sidebar:
     InterviewMind is a tool built on GPT-3 to automate the interview workflow. 
     """)
 
+def extract_text(file):
+    text = []
+    with pdfplumber.open(file) as pdf:
+        pages = pdf.pages
+        for p in pages:
+            text.append(p.extract_text())
+    return text
 
+uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
+if uploaded_file is not None:
+    text = extract_text(uploaded_file)
+    text = ''.join(text)
+    #text.replace('\n', '  \n')
+    st.info(text)
+
+_ = '''
 input_text = None
 if 'output' not in st.session_state:
     st.session_state['output'] = 0
 
 if st.session_state['output'] <=2:
     st.markdown("""
-    # Interview Questions
+    # Enter a prompt here
     """)
     input_text = st.text_input("Prompt")
-    st.session_state['output'] = st.session_state['output'] + 1
+    st.session_state['output'] += 1
 else:
     st.info("Refresh for another prompt")
 
-hide="""
-<style>
-footer{
-	visibility: hidden;
-    position: relative;
-}
-.viewerBadge_container__1QSob{
-    visibility: hidden;
-}
-#MainMenu{
-	visibility: hidden;
-}
-<style>
-"""
-st.markdown(hide, unsafe_allow_html=True)
-
-st.markdown(
-    """
-    <style>
-        iframe[width="220"] {
-            position: fixed;
-            bottom: 60px;
-            right: 40px;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
 if input_text:
-    prompt = "prompt"
+    prompt = input_text
     if prompt:
         openai.api_key = "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"
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt="Create a list of 8 questions for a software engineering job interview",
+            prompt=prompt,
             temperature=0.5,
             max_tokens=150,
             top_p=1.0,
@@ -73,3 +54,4 @@ if input_text:
         )
         output = response['choices'][0]['text']
         st.info(output)
+'''
