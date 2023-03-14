8bd5128b13eea6e600194bbd1ea35ed19d028d57
Eric Zeng <47470180+ericio123@users.noreply.github.com>
2023-03-13 23:09:00 -0700

    Add profile creation

--- CS324/InterviewMind.py
+++ CS324/InterviewMind.py
@@ -10,6 +10,8 @@ with st.sidebar:
     InterviewMind is a tool built on GPT-3 to automate the interview workflow. 
     """)
 
+st.session_state.pdf = 0
+
 def extract_text(file):
     text = []
     with pdfplumber.open(file) as pdf:
@@ -22,8 +24,69 @@ uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
 if uploaded_file is not None:
     text = extract_text(uploaded_file)
     text = ''.join(text)
-    #text.replace('\n', '  \n')
     st.info(text)
+    st.session_state.pdf = 1
+    openai.api_key = "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"
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Answer with the name of this person based on their resume: " + text,
+        temperature=0.5,
+        max_tokens=150,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+    )
+    output = response['choices'][0]['text']
+    st.info(output)
+    st.session_state.name = output
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Answer with the college, major, and GPA of this person based on their resume: " + text,
+        temperature=0.5,
+        max_tokens=150,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+    )
+    output = response['choices'][0]['text']
+    st.info(output)
+    st.session_state.education = output
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Answer with the work history of this person based on their resume: " + text,
+        temperature=0.5,
+        max_tokens=150,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+    )
+    output = response['choices'][0]['text']
+    st.info(output)
+    st.session_state.work = output
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Answer with the technical skills of this person based on their resume: " + text,
+        temperature=0.5,
+        max_tokens=150,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+    )
+    output = response['choices'][0]['text']
+    st.info(output)
+    st.session_state.skills = output
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Answer with the contact information of this person based on their resume: " + text,
+        temperature=0.5,
+        max_tokens=150,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+    )
+    output = response['choices'][0]['text']
+    st.info(output)
+    st.session_state.contact = output
 
 _ = '''
 input_text = None
