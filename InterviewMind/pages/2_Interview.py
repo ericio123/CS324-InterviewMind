edfc32d2a8beeb2851479943f3a7e3dee14f4c16
Eric Zeng <ericio123@gmail.com>
2023-03-15 01:26:30 -0700

    Reorder pages

--- InterviewMind/pages/2_Interview.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-if st.session_state.role == "Recruiter":
-    st.info("interview")
-else:
-    st.write("Sorry, applicants don't have access to this page. Please change your role on the home page if you want to use this feature.")
